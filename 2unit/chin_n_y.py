64a43287c6f4d4c5d28c37c65838cfe66f494a02
Ivan Lialiuk <lualgod92@gmail.com>
2022-07-03 12:47:32 +0300

    test

---
+++ 2unit/chin_n_y.py
@@ -0,0 +1,13 @@
+from pickle import APPEND
+from unittest import result
+
+
+year = int(input())
+
+years = ['Обезьяна', 'Петух', 'Собака', 'Свинья', 'Крыса', 'Бык', 'Тигр', 'Заяц', 'Дракон', 'Змея', 'Лошадь', 'Овца']
+
+result = year % 12
+
+print(years.index)
+# for res in years:
+#     res = year % 12
